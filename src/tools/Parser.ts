ecb6d9290c35c2c1511a17c28c85870bc534ea8c
Alesander López Gil <ales.sander93@gmail.com>
2020-10-24 13:46:29 +0100

    Restored Parser Buffer

--- src/tools/Parser.ts
+++ src/tools/Parser.ts
@@ -2,6 +2,9 @@
 const maybeJSON = (text) => {
   try {
     let response = JSON.parse(text);
+    if (response && response.Body) {
+      response.Body = Buffer.from(response.Body);
+    }
     return response;
   } catch (e) {
     return text;
